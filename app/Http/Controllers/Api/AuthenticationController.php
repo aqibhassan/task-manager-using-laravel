7060769abacf84168cbc87d252457a36b98f34fe
Aqib Hassan <aqibhassan@Aqibs-MacBook-Pro.local>
2023-10-11 16:16:58 +0100

    Task Controller path updated

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -7,9 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
-
-
-
 class AuthenticationController extends Controller
 {
 
--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Task;
-use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use App\Http\Requests\TaskRequest;
 
